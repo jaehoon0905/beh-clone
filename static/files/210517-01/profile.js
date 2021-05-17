6752bc4feb3bd71914aefcf997f270a316af9048
JAEHOON <jaehoonlee@gc.gachon.ac.kr>
2021-05-17 16:39:30 +0900

    210517 Ajax - Profile page

--- static/files/210517-01/profile.js
+++ static/files/210517-01/profile.js
@@ -1,3 +1,15 @@
+if (location.href.split("?").length > 1) {
+  const param = JSON.parse(
+    '{"' +
+      decodeURI(
+        location.href.split("?")[1].replace(/&/g, '","').replace(/=/g, '":"')
+      ) +
+      '"}'
+  );
+} else {
+  const param = { uId: 1 };
+}
+
 const xhr = [new XMLHttpRequest(), new XMLHttpRequest()];
 const json = [];
 xhr[0].open("GET", "user.json");
@@ -12,7 +24,7 @@ xhr[0].onreadystatechange = function () {
   if (xhr[0].readyState !== XMLHttpRequest.DONE) return;
 
   if (xhr[0].status === 200) {
-    json[0] = JSON.parse(xhr[0].responseText[0]);
+    json[0] = JSON.parse(xhr[0].responseText[param.uId]);
 
     document.querySelector(".profile--name").innerHTML = json[0].name;
     document.querySelector(".profile--status").innerHTML = json[0].postion;
@@ -30,28 +42,31 @@ xhr[1].onreadystatechange = function () {
   if (xhr[1].status === 200) {
     json[1] = JSON.parse(xhr[1].responseText);
     json[1].forEach((elem) => {
-      li = document.createElement("li");
-      a = document.createElement("a");
-      a.href = "view.html?id=" + elem.contentId;
-      img = document.createElement("img");
-      img.src = "files/" + elem.contentId + ".jpg";
-      img.alt = elem.contentId;
-      div = document.createElement("div").innerHTML = elem.name;
-      div.classList.add("works--detail");
-      tmp = document.createElement("span").innerHTML = elem.name;
-      tmp.classList.add("works--title");
-      div.appendChild(tmp);
-      tmp = document.createElement("ul").innerHTML = elem.name;
-      tmp.classList.add("works--tags");
-      elem.tags.forEach((el) => {
-        li = document.createElement("li").innerHTML = el;
-        tmp.appendChild(li);
-      });
+      if (elem.contentId > 0) {
+        li = document.createElement("li");
+        a = document.createElement("a");
+        a.href = "view.html?id=" + elem.contentId;
+        img = document.createElement("img");
+        img.src = "files/" + elem.contentId + ".jpg";
+        img.alt = elem.contentId;
+        div = document.createElement("div").innerHTML = elem.name;
+        div.classList.add("works--detail");
+        tmp = document.createElement("span").innerHTML = elem.name;
+        tmp.classList.add("works--title");
+        div.appendChild(tmp);
+        tmp = document.createElement("ul").innerHTML = elem.name;
+        tmp.classList.add("works--tags");
+        elem.tags.forEach((el) => {
+          li = document.createElement("li").innerHTML = el;
+          tmp.appendChild(li);
+        });
 
-      div.appendChild(tmp);
-      a.appendChild(img);
-      a.appendChild(div);
-      li.appendChild(a);
+        div.appendChild(tmp);
+        a.appendChild(img);
+        a.appendChild(div);
+        li.appendChild(a);
+        document.querySelector(".works").appendChild(li);
+      }
     });
   } else {
     console.log(`[${xhr[1].status}] : ${xhr[1].statusText}`);
